9a8dfabdd6ca604a82e8129f51df009fea418c85
Timo <timofatablet@gmail.com>
2020-10-08 19:16:55 -0700

    first draft on problem2 and test

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,7 +1,27 @@
 public class Problem2 {
-
+    //This function will sort an array in ASCENDING order.
     // Do not change signature (function name, parameters)
+
     public static void bubbleSort(int[] data) {
-        // Add your solution
+        //this statement forces a return if the array is empty or null
+        if (data == null || data.length == 0) {
+            return;
+        }
+        //this outer for loop allows the loop to swap larger numbers for smaller ones more than once
+        for (int i = 0; i < data.length; i++) {
+            //this loop goes through the entire array and swaps whichever large numbers are left adjacent
+            //to the smaller ones
+            for (int x = 0; x < data.length - 1; x++) {
+                //this if statement checks if the number to the right of the one being checked is smaller
+                if (data[x + 1] < data[x]) {
+                    //this int temporarily stores the array value needing to be swapped
+                    int temp = data[x];
+                    data[x] = data[x+1];
+                    data[x + 1] = temp;
+                }
+            }
+
+        }
     }
 }
+
--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -8,7 +8,35 @@ public class Problem2Test {
     // Okay to change
     @Test
     public void testBubbleSort() {
-        assertTrue(false);
-        // add your tests here
+        //assertTrue(false);
+        int[][] inputs = {
+                {},
+                {3},
+                {1, 3},
+                {6, 4, 8},
+                {4, 6, 8},
+                {4, 10, 8, 7, 3, 2, 1},
+                {3, 5, 7, 9, 20, 123, 4, 3, 14},
+                {3, 1, 7, 9, 20, 23, 10, 23, 15},
+        };
+        int[][] answers = {
+                {},
+                {3},
+                {1, 3},
+                {4, 6, 8},
+                {4, 6, 8},
+                {1, 2, 3, 4, 7, 8, 10},
+                {3, 3, 4, 5, 7, 9, 14, 20, 123},
+                {1, 3, 7, 9, 10, 15, 20, 23, 23},
+        };
+        for (int i = 0; i < inputs.length; i++) {
+            Problem2.bubbleSort(inputs[i]);
+            try {
+                assertArrayEquals(answers[i], inputs[i]);
+            } catch (AssertionError error) {
+                System.out.println(" Test Case " + i + " failed " + error);
+            }
+
+        }
     }
 }
